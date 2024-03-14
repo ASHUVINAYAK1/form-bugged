8066fee9b33122493dbb0136ce3bd344a136a6bd
rakeshgowda123 <rakeshgowder251999@gmail.com>
2024-03-14 14:46:21 +0530

    bug cleared

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Form from './components/Form';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <ToastContainer position = "top-left"/>
     </div>
+    
   );
 }
 
